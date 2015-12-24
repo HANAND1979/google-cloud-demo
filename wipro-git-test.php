d7f7bbd7f5784c7501a5d1ea241fc8fb72af2e16
HANAND1979 <HANAND1979@users.noreply.github.com>
2015-12-24 14:24:21 +0530

    Create wipro-git-test.php

---
+++ wipro-git-test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<img src="https://storage.googleapis.com/wipro-demo/images/WCC_Logo.gif" height="100px"/>
+<h1>Welcome to the Google Cloud Demo at Wipro-WSIC with Git</h1>
+<h2>Powered by GAFA League</h2>
+<?php
+echo("<p>This is a demo of a load balanced Google Cloud web deployment. This demo is running on a Managed Instance Group with Auto-scaling enabled.<br/>
+The URL you are accessing is hitting the Google Cloud Load Balancer and distributing the load to managed VMs in the backend.</p>");
+?>
+<img src="https://storage.googleapis.com/wipro-demo/images/Picture1.png" height="400px"/>
+</html>
