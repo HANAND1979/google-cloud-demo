87f827176411d57117cc565a183bf31cb319821e
HANAND1979 <HANAND1979@users.noreply.github.com>
2015-12-24 18:56:21 +0530

    Update wipro-git-test.php

--- wipro-git-test.php
+++ wipro-git-test.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <img src="https://storage.googleapis.com/wipro-demo/images/WCC_Logo.gif" height="100px"/>
-<h1>Welcome to the Google Cloud Demo at Wipro-WSIC with Git</h1>
+<h1>Welcome to the Google Cloud Demo - Demo of GitHub integration and Autoscaling with startup script</h1>
 <h2>Powered by GAFA League</h2>
 <?php
 echo("<p>This is a demo of a load balanced Google Cloud web deployment. This demo is running on a Managed Instance Group with Auto-scaling enabled.<br/>
